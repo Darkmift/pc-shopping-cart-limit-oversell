b2ce32541f92fafaba9fd66d772202d85722c230
Darkmift <darkmift@gmail.com>
2024-04-25 14:22:44 +0300

    Refactor code to add logger in main.ts and configure winston in logger.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
+import logger from '@/utils/logger';
 import app from './src/index';
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  logger.info(
+    'Server is running on http://localhost:3000',
+    { foo: 'bar' },
+    true,
+    55,
+  );
 });
