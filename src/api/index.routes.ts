868f02beadb94015448595b8c2be776c34aa9f91
Darkmift <darkmift@gmail.com>
2024-04-30 05:05:21 +0300

    Add routes and controllers for products, carts, and product inventory

--- src/api/index.routes.ts
+++ src/api/index.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import userRouter from './users/users.routes';
 import authRouter from './auth/auth.routes';
+import productsRouter from './products/products.router';
+import cartsRouter from './carts/carts.router';
+import productInventoryRouter from './product-inventory/product-inventory.router';
 
 const apiRouter: Router = Router();
 
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/users', userRouter);
+apiRouter.use('/products', productsRouter);
+apiRouter.use('/carts', cartsRouter);
+apiRouter.use('/product-inventory', productInventoryRouter);
 
 export default apiRouter;
