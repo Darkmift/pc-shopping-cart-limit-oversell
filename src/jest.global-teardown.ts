a80112f7155f6439144bf88d2d67d0950ad9280f
Darkmift <darkmift@gmail.com>
2024-04-30 05:03:56 +0300

    Refactor Jest global teardown to delete database tables

--- src/jest.global-teardown.ts
+++ src/jest.global-teardown.ts
@@ -1,7 +1,18 @@
 import 'tsconfig-paths/register';
+import db from '@/common/drizzle/db';
+import {
+  users,
+  carts,
+  products,
+  productsInventory,
+} from './common/drizzle/db/schema';
 import { poolConnection } from '@/common/drizzle/db';
 
 export default async () => {
+  await db.delete(productsInventory);
+  await db.delete(products);
+  await db.delete(carts);
+  await db.delete(users);
   console.log('Global teardown init');
   await poolConnection.end();
   console.log('Global teardown complete');
